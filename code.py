22a2ad2293dec850efc3b23f11e07aaaa30d56ed
jules3426 <88186715+jules3426@users.noreply.github.com>
2021-07-30 12:22:11 +0100

    Add files via upload

---
+++ code.py
@@ -0,0 +1,159 @@
+import time, random
+item1='none'
+item2='none'
+item3='none'
+print('welcome,')
+name=input(''''use
+
+
+please create a username:''')
+
+
+print('welcome,',name)
+
+
+while True:
+    A=input('''type in 'start' to start:''')
+    if A=='start':
+        break
+    else:
+        time.sleep(1)
+        print('''type in 'start' ''','not',''''''+ A,  '''''')
+        time.sleep(1)
+
+
+
+print('congratultions YOU CAN READ')
+time.sleep(2)
+print('shoot!! I forgot to tell you how to play this game...')
+time.sleep(5)
+print('''SHIT!!!! I can't find the right script... ''')
+time.sleep(2)
+print('*scrolling sounds*')
+time.sleep(5)
+print('*click sound* found it!')
+time.sleep(3)
+print('''
+           |
+           |__
+       t   |  | e
+
+          NoN gAme''')
+
+
+
+time.sleep(5)
+print('...')
+
+time.sleep(10)
+
+print('oh...   ...now a remember...   ...there is no game: no tetris for you, no minecraft, why not go outside its sunny and you know there is no game.')
+
+time.sleep(10)
+
+print('leave... ...you should leave')
+
+time.sleep(1)
+
+
+
+
+print('there is no game!!!!!!!!!!')
+
+
+
+A=input('''
+           |
+           |__
+       t   |  | e
+
+          NoN gAme''')
+time.sleep(1)
+A=input('hey what are you doing your destroying my sgn!')
+item1='i'
+time.sleep(2)
+print('you took the i from his sentence...')
+time.sleep(1)
+print('hey! can not use a')
+time.sleep(2)
+print('hey! can not use a')
+print('you basterd you took my i')
+time.sleep(1)
+print('??????????')
+time.sleep(1)
+print('i found an I *boasting*')
+time.sleep(5)
+A=input('''
+           |
+           |__
+       t   |  | e
+
+          NoN gAme''')
+
+
+A=input('would you like to use your i?')
+print('you used your i')
+time.sleep(3)
+print('from now on he is called computer')
+time.sleep(3)
+print('computer: hey, what are YOU DOING? you unscrewed my sign')
+time.sleep(4)
+
+
+
+
+
+
+
+
+
+
+
+A=input('''
+
+       play the game
+       
+      /ghjdkbfsgdjkf ''')
+
+
+time.sleep(5)
+print('computer: stop there is no game you can not play it')
+
+time.sleep(5)
+print('     +==========+')
+time.sleep(1)
+print('     ============')
+print('     the non game')
+time.sleep(1)
+print('     ============')
+print('     +==========+')
+
+time.sleep(3)
+A=input('computer: thee is no game!!!!!!!')
+print('you took the r from his sentence')
+item2='r'
+A=input('would you like to craft something?')
+while True:
+    time.sleep(1)
+    A=input('what is the first thing you would like to craft(r or i):')
+    if A=='i':
+        S=False
+    else:
+        S=True
+
+    print('your last ingredent is i or r')
+    print('you add your last ingredent')
+    time.sleep(5)
+    if S==True:
+        print('you succeded!')
+        print('you made a drill')
+        break
+
+    else:
+        print('you failed try again')
+
+
+
+item1='drill'
+item2='none'
+   
